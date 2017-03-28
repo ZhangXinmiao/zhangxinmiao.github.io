aab4e12f442189713409b02d22e5469539d3d0b0
张鑫淼 <zhangxinmiao02@meituan.com>
2017-03-28 23:07:08 +0800

    Site updated: 2017-03-28 23:07:08

--- js/main.js
+++ js/main.js
@@ -80,13 +80,4 @@ $(document).ready(function(){
     }
     });
   }
-
-    var _hmt = _hmt || [];
-    (function() {
-      var hm = document.createElement("script");
-      hm.src = "https://hm.baidu.com/hm.js?6758d90e17524a26c9e187195678a132";
-      var s = document.getElementsByTagName("script")[0];
-      s.parentNode.insertBefore(hm, s);
-    })();
-
 });
